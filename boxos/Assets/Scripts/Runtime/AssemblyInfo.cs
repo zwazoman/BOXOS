e597a0cf0660c899f3918cfd51e3957d2a879c5a
zwazoman <gloupik.lemaire@gmail.com>
2025-07-06 17:57:49 +0200

    tt

--- boxos/Assets/Scripts/Runtime/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-using System.Runtime.CompilerServices;
-//Allows internals visibility for testing purposes
-[assembly: InternalsVisibleTo("Unity.Template.Multiplayer-NGO.Tests.Runtime")]
--- boxos/Assets/Scripts/Runtime/Shared/Events.cs
+++
@@ -1,7 +0,0 @@
-namespace Unity.Template.Multiplayer.NGO.Runtime
-{
-    /// <summary>
-    /// Called when a match is entered (I.E: after matchmaking finds enough players)
-    /// </summary>
-    internal class MatchEnteredEvent : AppEvent { }
-}
